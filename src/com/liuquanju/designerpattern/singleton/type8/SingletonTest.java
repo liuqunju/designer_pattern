99e2ca0bd71899a7c792bd5e0181f53bd3acd76d
liuquanju <yingjie.liu@outlook.com>
2020-01-04 17:30:33 +0800

    update

--- src/com/liuquanju/designerpattern/singleton/type8/SingletonTest.java
+++ src/com/liuquanju/designerpattern/singleton/type8/SingletonTest.java
@@ -10,6 +10,7 @@ public class SingletonTest {
         System.out.println(singleton1 ==singleton2);
         System.out.println(singleton1.hashCode());
         System.out.println(singleton2.hashCode());
+        
     }
 
 }
